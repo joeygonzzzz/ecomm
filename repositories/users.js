6f2e117e6dd46592826327d0104ca0a16c51d069
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-18 22:16:38 -0500

    Updating user repo, added password

--- repositories/users.js
+++ repositories/users.js
@@ -54,12 +54,24 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`)
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('fefb09ad');
+    await repo.update("91bdc409", { password: 'mypassword'});
 };
 
 test();
