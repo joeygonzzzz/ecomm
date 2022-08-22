796f09909f4eb1d690b09d6f01264048b409acc6
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-21 19:22:16 -0500

    Fetching a session to user json

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(
+    cookieSession({
+     keys: ['ffgklsjdhfgklsdfg']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
     <div>
+        Your id is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password" placeholder="password" />
@@ -33,6 +39,8 @@ app.post('/', async (req, res) => {
     }
 
     const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
 
     res.send('Account created!');
 });
