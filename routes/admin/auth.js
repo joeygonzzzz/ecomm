42f96e6629fb36685f7e9f5b997ab2dc56339689
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-09-01 20:55:02 -0500

    adding middleware and cleaning out error code

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middleware');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -11,6 +11,7 @@ const {
     requireEmailExists,
     requireValidPasswordForUser 
 } = require('./validators');
+const signin = require('../../views/admin/auth/signin');
 
 const router = express.Router();
 
@@ -20,20 +21,15 @@ router.get('/signup', (req, res) => {
 
 router.post(
     '/signup',
-    [requireEmail, requirePassword, requirePasswordConfirmation, ], 
+    [requireEmail, requirePassword, requirePasswordConfirmation], 
+    handleErrors(signupTemplate)
     async (req, res) => {
-        const errors = validationResult(req);
-        
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors}));
-        }
+        const { email, password } = req.body;
+        const user = await usersRepo.create({ email, password });
 
-    const { email, password, passwordConfirmation } = req.body;
-    const user = await usersRepo.create({ email, password });
+        req.session.userId = user.id;
 
-    req.session.userId = user.id;
-
-    res.send('Account created!');
+         res.send('Account created!');
 });
 
 router.get('/signout', (req, res) => {
@@ -45,18 +41,11 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', 
-    [
-        requireEmailExists,
-        requireValidPasswordForUser
-    ],
+router.post(
+    '/signin', 
+    [requireEmailExists, requireValidPasswordForUser],
+    handleErrors(signinTemplate)
     async (req, res) => {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.send(signinTemplate({ errors }))
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email});
