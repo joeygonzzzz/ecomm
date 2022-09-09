383a353ef41afe93665f81f0b3a604b7761a9168
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-09-08 21:15:42 -0500

    basic submission option and test

--- routes/carts.js
+++ routes/carts.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/cart/products', (req, res) => {
+    console.log(req.body.productId);
 
+    res.send('Product added to cart');
+})
 
 module.exports = router;
