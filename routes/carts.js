b2f9eb8eed643773c49ffd0a4448080ba353737d
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-09-08 22:27:14 -0500

    able to add items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const carts = require('../repositories/carts');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
-router.post('/cart/products', (req, res) => {
-    console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+
+    let cart;
+    if (!req.session.cartId) {
+
+
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+
+        cart.items.push({ id: req.body.productId, quantity: 1});
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
 
     res.send('Product added to cart');
 })
